c0f35ced3601dae3912fa1483224ed41b1695272
Robert Henderson <robert@nterpryz.com>
2018-01-30 15:53:51 +0000

    assigned play buttons and text view in Play onCreate method

--- app/src/main/java/com/roberthenderson/snap/Play.java
+++ app/src/main/java/com/roberthenderson/snap/Play.java
@@ -2,12 +2,25 @@ package com.roberthenderson.snap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Play extends AppCompatActivity {
+
+    Button player1Button;
+    Button player2Button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+
+        player1Button = findViewById(R.id.player1Button);
+        player2Button = findViewById(R.id.player2Button);
+        textView = findViewById(R.id.textView);
     }
+
+
 }
